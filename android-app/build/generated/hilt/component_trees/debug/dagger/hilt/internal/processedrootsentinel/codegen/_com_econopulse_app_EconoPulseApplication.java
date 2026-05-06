0eccf8a017e0bd944797031f782262fac9fbb89d
User <user@example.com>
2026-05-06 14:34:58 +0200

    chore(mobile): remove legacy android-app, add Capacitor release signing config

--- android-app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_econopulse_app_EconoPulseApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.econopulse.app.EconoPulseApplication"
-)
-public final class _com_econopulse_app_EconoPulseApplication {
-}
